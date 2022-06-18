e543fadc3a11b2282e31ba7a0a29142e2b1e99c5
Marius115 <cascaranomeister@gmail.com>
2022-06-18 21:56:02 +0000

    Agregado counter layout. Inactivo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,17 +5,7 @@ import React from "react";
 //create your first component
 const Home = () => {
 	return (
-		function SimpleCounter(props){
-			return (<div className="BigCounter">
-				
-				<div className="calendar"></div>
-				<div className="four"></div>
-				<div className="three"></div>
-				<div className="two"></div>
-				<div className="one"></div>
-				
-			</div>);
-		}
+		<p>hello world</p>
 	);
 };
 
--- src/js/index.js
+++ src/js/index.js
@@ -5,8 +5,35 @@ import ReactDOM from "react-dom";
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
-//import your own components
-import Home from "./component/home.jsx";
+function SimpleCounter(props){
+    return (<div className="bigCounter">
+        
+        <div className="calendar">
+        <i className="fa-solid fa-clock"></i>
+        </div>
+        <div className="four">{props.digitFour}</div>
+        <div className="three">{props.digitThree}</div>
+        <div className="two">{props.digitTwo}</div>
+        <div className="one">{props.digitOne}</div>
+        
+    </div>);
+}
+
+let counter = 0;
+setInterval(function() { 
+    const Four = Math.floor(counter/10000);
+    const Three = Math.floor(counter/1000);
+    const Two = Math.floor(counter/100);
+    const One = Math.floor(counter/10);
+    console.log(F)
+
+
+    ReactDOM.render(
+        <SimpleCounter digitOne={counter.indexOf()} />,
+        document.querySelector('#app')
+    );
+},100);
+
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<SimpleCounter />, document.querySelector("#app"));
